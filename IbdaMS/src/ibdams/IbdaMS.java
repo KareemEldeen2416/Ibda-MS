a7339f65ebf532743aeb16f89f1e35515d7aaba6
KareemEldeen Ahmed <kareemeldeenahmed@gmail.com>
2025-04-14 23:58:49 +0200

    first stage

--- IbdaMS/src/ibdams/IbdaMS.java
+++ IbdaMS/src/ibdams/IbdaMS.java
@@ -4,17 +4,34 @@
  */
 package ibdams;
 
+import java.io.IOException;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
 /**
  *
  * @author KareemEldeen
  */
-public class IbdaMS {
+public class IbdaMS extends Application{
+    
+    @Override
+    public void start(Stage stage) throws IOException{
+        Parent root = FXMLLoader.load(getClass().getResource("/windows/login.fxml"));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setTitle("Login-Ibda Academy MS");
+        stage.getIcons().add(new Image(getClass().getResourceAsStream("ibdalogo.png")));
+        stage.setResizable(false);
+        stage.show();
+    }
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        launch(args);
     }
     
 }
